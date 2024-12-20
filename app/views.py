9d4225f77a5119b3fc7db8791b41289e11081205
Chris Skeen <chrisskeen2006@gmail.com>
2024-12-20 11:01:22 -0600

    small changes - added logout and home page

--- app/views.py
+++ app/views.py
@@ -11,9 +11,10 @@ from app.models import *
 
 # Create your views here.
 
-def login_view(request):
-  return render(request, "log-in.html", {})
+# Sign-up / Log-in area -----------------------------------------------------------------------------------
 
+
+@unauthenticated_user
 def signup_view(request):
   form = CreateUserForm()
   
@@ -29,3 +30,29 @@ def signup_view(request):
       # user.groups.add(group)
       return redirect('login')
   return render(request, 'sign-up.html', {"form": form})
+
+
+@unauthenticated_user
+def login_view(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.info(request, 'Username OR password is incorrect.')
+  return render(request, "log-in.html", {})
+
+
+@login_required(login_url='login')
+def logout_view(request):
+  logout(request)
+  return redirect('login')
+# ---------------------------------------------------------------------------------------------------
+
+
+@login_required(login_url='login')
+def home_view(request):
+  return render(request, 'home.html', {})
--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,12 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # user-based
     path("login", login_view, name="login"),
-    path("signup", signup_view, name="signup")
+    path('logout', logout_view, name="logout"),
+    path("signup", signup_view, name="signup"),
+
+    # main pages
+    path("home", home_view, name="home"),
 ]
